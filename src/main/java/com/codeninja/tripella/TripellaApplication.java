8e9a75aee43eac9886cef7df826bd204adbc14c4
Abdulrahman Elyas <arelyas@posteo.net>
2021-02-20 18:45:35 +0300

    initialization

---
+++ src/main/java/com/codeninja/tripella/TripellaApplication.java
@@ -0,0 +1,13 @@
+package com.codeninja.tripella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripellaApplication.class, args);
+	}
+
+}

e99089dcf4a19c3581664ae5e5e90635bddca44f
KhawlahAlorini <khawlasal7@gmail.com>
2021-02-21 19:14:10 +0300

    review and detail controller

--- src/main/java/com/codeninja/tripella/model/Trip.java
+++ src/main/java/com/codeninja/tripella/model/Trip.java
@@ -11,7 +11,6 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 
 @Entity
 public class Trip {

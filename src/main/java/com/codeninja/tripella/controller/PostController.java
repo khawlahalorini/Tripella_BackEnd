b048b2400b4bdf776f7bdb1814499c4b757510af
KhawlahAlorini <khawlasal7@gmail.com>
2021-03-02 12:13:25 +0300

    fix

--- src/main/java/com/codeninja/tripella/controller/PostController.java
+++ src/main/java/com/codeninja/tripella/controller/PostController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import com.codeninja.tripella.model.Post;
 import com.codeninja.tripella.service.PostService;
 
